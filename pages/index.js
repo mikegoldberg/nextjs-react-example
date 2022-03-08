2cdf3c3297e72f2f5a1bee0d3229d13863294260
Your Name <mike@webkitstudio.com>
2022-03-07 18:55:23 -0700

    feature: blurbs component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import "../components/icons";
 import Profile from "../components/profile";
+import Blurbs from "../components/blurbs";
 import Contact from "../components/contact";
 import Interests from "../components/interests";
 import NetworkRelation from '../components/networkRelation';
@@ -23,6 +24,7 @@ export default function Home() {
           </Col>
           <Col>
             <NetworkRelation />
+            <Blurbs />
           </Col>
         </Row>
       </main>
